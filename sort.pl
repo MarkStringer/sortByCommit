858061ec42d416e005302d01e84b9df0346eb3d6
Mark Stringer <mark.stringer@mumbly.co.uk>
2017-01-08 08:55:48 +0000

    Get the sort by commit working

--- sort.pl
+++ sort.pl
@@ -1,5 +1,24 @@
+use strict;
+use Cwd;
+use Git;
+
+sub commitDate
+  {
+      my $file = $_[0];
+      my $version = Git::command_oneline('log', '-1', '--format=%ct', $file);
+      return $version;
+  }
+
 ## read in text files
+sub sortByCommit
+  {
+  	my $dir = $_[0]; ## argument passed in
+  	opendir (DIR, $dir)||die "Failed to open target directory";
+  	my @dir= grep { $_ ne '.' && $_ ne '..' && $_=~/\.txt$/}  readdir(DIR);
+  	closedir(DIR);
+	@dir = sort { commitDate($b) <=> commitDate($a) } (@dir);
 
-## sort them by their commit date
+  return @dir;	  
+  }
 
-## print out the result
+print sortByCommit(getcwd); 
